1f0e14665910933c73d771d38596e29bf189df79
Princykumari <princyaanand293@gmail.com>
2024-12-08 20:03:14 +0530

    checkingEvenOdd

---
+++ index.js
@@ -0,0 +1,10 @@
+let num = 3;
+ function checkPrime(num){
+    for (let i=2;i<N-1;i++){
+        if (num%i==0){
+            return false
+        }
+    }
+    return true
+ }
+ console.log(checkPrime(6))

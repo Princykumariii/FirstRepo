63a0df47714c3798369cca702e5a4c1371af0506
Your GitHub Username <Your Email>
2025-01-08 23:14:07 +0530

    es6

---
+++ object/index.js
@@ -0,0 +1,14 @@
+class Book {
+   
+    constructor(title,author,year,isavailable) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
+
+const totalBooks = new Car ("Toyota", "Corolla");
+const car2 = new Car("Honda", "Civic");
+
+console.log(car1.make);  // "Toyota"
+console.log(car2.make);  // "Honda"
